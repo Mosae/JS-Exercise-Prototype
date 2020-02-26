58d1160cb18aa1ea08f83e4dd2ba6f766d17bb03
Mosae Litsoane <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-02-26 17:44:18 -0600

    baby subclassing method added- 19passing

--- index.js
+++ index.js
@@ -46,10 +46,18 @@ function Person(name, age) {
 }
 
 Person.prototype.eat = function (someFood) {
-  if (this.stomach.length <= 10) {
+  if (this.stomach.length < 10) {
     return this.stomach.push(someFood)
   }
 
+}
+
+Person.prototype.poop = function () {
+  this.stomach = []
+}
+
+Person.prototype.toString = function () {
+  return `${this.name} ${this.age}`;
 }
 
 /*
@@ -66,9 +74,17 @@ Person.prototype.eat = function (someFood) {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model,
+    this.milesPerGallon = milesPerGallon
+  this.tank = 0,
+    this.odometer = 0
 }
+
+Car.prototype.fill = function (gallons) {
+  this.tank += gallons;
+}
+
 
 /*
   TASK 3
@@ -77,7 +93,9 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 
 }
 
